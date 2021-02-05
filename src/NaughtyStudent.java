a9ee4f388baf57c50e629aeff3e12fb76d569f5d
AlhajiTaibu <alhabdutaib@gmail.com>
2021-02-05 10:39:25 +0000

    Only inheritance

--- src/NaughtyStudent.java
+++ src/NaughtyStudent.java
@@ -1,7 +1,4 @@
 public class NaughtyStudent extends Student {
-//    public NaughtyStudent(String name){
-//        super(name);
-//    }
     public double getAverageGrade(){
         return super.getAverageGrade()*1.10;
     }
